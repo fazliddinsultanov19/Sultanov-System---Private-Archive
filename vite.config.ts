56ac05b7c21323a24c29a2f455edf42c3fcc7267
Fazliddin <fazliddinsultanov19@gmail.com>
2025-12-26 01:41:11 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: "/"
+    base: "/",
     const env = loadEnv(mode, '.', '');
     return {
       server: {
